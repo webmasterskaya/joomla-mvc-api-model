8aada49c28eade184b8369d35384c367807e6efc
Artem Vasilev <kern.usr@gmail.com>
2022-08-05 13:16:47 +0300

    Add trait with setter and getter of PSR-7 http client

--- src/MVC/Model/APIAwareTrait.php
+++ src/MVC/Model/APIAwareTrait.php
@@ -9,7 +9,44 @@
 
 namespace Webmasterskaya\Joomla\MVC\Model;
 
+use Psr\Http\Client\ClientInterface;
+
 trait APIAwareTrait
 {
 
+	/**
+	 * The HTTP client.
+	 *
+	 * @var    ClientInterface
+	 */
+	protected $_client;
+
+
+	/**
+	 * Get the HTTP client.
+	 *
+	 * @throws  \UnexpectedValueException
+	 * @return  ClientInterface  The HTTP client.
+	 */
+	public function getHTTPClient()
+	{
+		if ($this->_client)
+		{
+			return $this->_client;
+		}
+
+		throw new \UnexpectedValueException('HTTP Client not set in ' . __CLASS__);
+	}
+
+	/**
+	 * Set the database driver.
+	 *
+	 * @param   ClientInterface  $client  The HTTP client.
+	 *
+	 * @return  void
+	 */
+	public function setHTTPClient(ClientInterface $client = null)
+	{
+		$this->_client = $client;
+	}
 }
