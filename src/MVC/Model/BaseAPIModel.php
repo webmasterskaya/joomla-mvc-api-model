a935fd5bdfd183c9246b5ebe1b9b5ef2d84ec4c7
Artem Vasilev <kern.usr@gmail.com>
2022-08-05 15:31:38 +0300

    Create and get endpoint object

--- src/MVC/Model/BaseAPIModel.php
+++ src/MVC/Model/BaseAPIModel.php
@@ -10,6 +10,7 @@
 namespace Webmasterskaya\Joomla\MVC\Model;
 
 use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Extension\ComponentInterface;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Http\HttpFactory;
 use Joomla\CMS\Language\Text;
@@ -17,8 +18,11 @@ use Joomla\CMS\MVC\Factory\MVCFactoryAwareTrait;
 use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use Joomla\CMS\MVC\Factory\MVCFactoryServiceInterface;
 use Joomla\CMS\MVC\Model\BaseModel;
+use Joomla\CMS\Uri\Uri;
 use Joomla\Event\DispatcherAwareInterface;
 use Joomla\Event\DispatcherAwareTrait;
+use Joomla\Event\DispatcherInterface;
+use Joomla\Event\EventInterface;
 
 abstract class BaseAPIModel extends BaseModel implements APIModelInterface, DispatcherAwareInterface
 {
@@ -37,6 +41,10 @@ abstract class BaseAPIModel extends BaseModel implements APIModelInterface, Disp
 	 * @var    string
 	 */
 	protected $event_clean_cache = null;
+
+	protected $__endpoint;
+
+	protected $endpoint;
 
 	/**
 	 * Constructor
@@ -88,11 +96,97 @@ abstract class BaseAPIModel extends BaseModel implements APIModelInterface, Disp
 			return;
 		}
 
+		if (!empty($config['endpoint']))
+		{
+			$this->endpoint = $config['endpoint'];
+		}
+
 		$component = Factory::getApplication()->bootComponent($this->option);
 
 		if ($component instanceof MVCFactoryServiceInterface)
 		{
 			$this->setMVCFactory($component->getMVCFactory());
+		}
+	}
+
+	protected function _createEndpoint($endpoint)
+	{
+
+
+		if (\is_array($endpoint) || \is_string($endpoint))
+		{
+			$uri = new URI();
+
+			if (\is_string($endpoint))
+			{
+				$uri->parse($endpoint);
+
+				return $uri;
+			}
+
+			if (count($endpoint) < 2)
+			{
+				throw new \ArgumentCountError('Array of endpoint params must have a 2 items, given ' . count($endpoint));
+			}
+
+			$uri->setHost($endpoint[0]);
+			$uri->setPath($endpoint[1]);
+
+			return $uri;
+		}
+
+		if ($endpoint instanceof URI)
+		{
+			return $endpoint;
+		}
+
+		throw new \InvalidArgumentException('Unsupported endpoint type', 0);
+	}
+
+	public function getEndpoint($endpoint = null)
+	{
+		if (empty($endpoint) && empty($this->__endpoint))
+		{
+			$endpoint = $this->__endpoint;
+		}
+
+		if (!empty($endpoint) && !!($_endpoint = $this->_createEndpoint($endpoint)))
+		{
+			return $_endpoint;
+		}
+
+		throw new \Exception('Endpoint not set', 0);
+	}
+
+	/**
+	 * Boots the component with the given name.
+	 *
+	 * @param   string  $component  The component name, eg. com_content.
+	 *
+	 * @throws \Exception
+	 * @return  ComponentInterface  The service container
+	 */
+	protected function bootComponent($component): ComponentInterface
+	{
+		return Factory::getApplication()->bootComponent($component);
+	}
+
+	/**
+	 * Dispatches the given event on the internal dispatcher, does a fallback to the global one.
+	 *
+	 * @param   EventInterface  $event  The event
+	 *
+	 * @return  void
+	 */
+	protected function dispatchEvent(EventInterface $event)
+	{
+		try
+		{
+			$this->getDispatcher()->dispatch($event->getName(), $event);
+		}
+		catch (\UnexpectedValueException $e)
+		{
+			Factory::getContainer()->get(DispatcherInterface::class)->dispatch($event->getName(), $event);
 		}
 	}
 }
